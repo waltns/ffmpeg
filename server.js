e15f48df3fc3d12bd33eb2c8bf3b3cc5d4ff0ace
Walt Den <waltwassire@gmail.com>
2026-06-20 23:57:21 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const { exec } = require("child_process");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({
-    status: "online"
+  exec("ffmpeg -version", (err, stdout, stderr) => {
+    if (err) {
+      return res.json({
+        ffmpeg: false,
+        error: stderr
+      });
+    }
+
+    res.json({
+      ffmpeg: true,
+      version: stdout
+    });
   });
 });
 
